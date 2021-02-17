b669855f42b1cf31bc943a099a23027b47217171
Varun Gollapalli <gvarun1999@gmail.com>
2021-02-17 16:50:04 +0530

    Update DoesScontainsT.java

--- Data Structures(Test-1)/DoesScontainsT.java
+++ Data Structures(Test-1)/DoesScontainsT.java
@@ -1,26 +1,25 @@
-
 public class Solution {
-	public static boolean checkSequence(String a, String b) {
-	if(a.length()<b.length())
-		{
-			return false;
-		}
-		int count = 0;
-		for(int i =0;i<a.length();i++)
-		{
-			if(count<b.length() && a.charAt(i)==b.charAt(count))
-			{
-				count++;
-			}
-		}
-		if(count==b.length())
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-		
-	}
+    public static boolean checkSequence(String a, String b) {
+        if(a.length()<b.length())
+        {
+            return false;
+        }
+        if(b.length()==0)
+        {
+            return true;
+        }
+        if(a.length()==0)
+        {
+            return false;
+        }
+
+        if(a.charAt(0)==b.charAt(0))
+        {
+            return checkSequence(a.substring(1),b.substring(1));
+        }
+        else
+        {
+            return checkSequence(a.substring(1),b);
+        }
+    }
 }
