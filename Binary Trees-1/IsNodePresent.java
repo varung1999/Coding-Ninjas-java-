e91571870bcbf7e04fed84b96f4a0e18fd13446f
Varun Gollapalli <gvarun1999@gmail.com>
2021-03-09 15:40:57 +0530

    Update IsNodePresent.java

--- Binary Trees-1/IsNodePresent.java
+++ Binary Trees-1/IsNodePresent.java
@@ -51,3 +51,23 @@ public class Solution {
         return ans;
     }
 }
+
+// best solution:
+
+/* public class Solution {
+
+    public static boolean isNodePresent(BinaryTreeNode<Integer> root, int x)
+    {
+        if(root==null)
+        {
+            return false;
+        }
+       
+        if(root.data==x)
+        {
+            return true;
+        }
+        
+        return (isNodePresent(root.left,x) || isNodePresent(root.right,x));
+    }
+} */
