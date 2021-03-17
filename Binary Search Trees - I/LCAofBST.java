b136e53dba141c5ec1f675351f9b87f35b4ad194
Varun Gollapalli <gvarun1999@gmail.com>
2021-03-17 19:23:38 +0530

    Update LCAofBST.java

--- Binary Search Trees - I/LCAofBST.java
+++ Binary Search Trees - I/LCAofBST.java
@@ -11,32 +11,20 @@ public class Solution {
 
 	
 	public static int getLCA(BinaryTreeNode<Integer> root, int a, int b) {
-	if(root==null) {
-			return -1;
-		}
-		if(a < root.data && b < root.data ) {
-			return getLCA(root.left, a, b);
-		}
-		if(a>root.data && b > root.data) {
-			return getLCA(root.right, a, b);
-		}
-		if(root.data == a || root.data == b) {
-			return root.data;
-		}
-		int leftLca = getLCA(root.left, a, b);
-		int rightLca = getLCA(root.right, a, b);
-		if(leftLca==-1 && rightLca==-1) {
-			return -1;
-		}
-		else if(leftLca==-1) {
-			return rightLca;
-		}
-		else if(rightLca==-1) {
-			return leftLca;
-		}
-		else {
-			return root.data;
-		}
 		
+        if(root==null)
+        {
+            return -1;
+        }
+        
+        if(a<root.data && b<root.data)
+        {
+            return getLCA(root.left,a,b);
+        }
+        if(a>root.data && b>root.data)
+        {
+            return getLCA(root.right,a,b);
+        }
+        return root.data;
 	}
 }
